95ca1b023c6596e6f65f9213c4f34cfff2c61fad
cindyz8735 <cindyz8735@gmail.com>
2017-01-04 18:53:56 -0800

    countdown

--- SM Hacks/ViewControllers/HomeViewController.swift
+++ SM Hacks/ViewControllers/HomeViewController.swift
@@ -10,12 +10,40 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    let formatter = DateFormatter()
+    let userCalendar = Calendar.current;
+    let requestedComponent : Set<Calendar.Component> = [
+        Calendar.Component.month,
+        Calendar.Component.day,
+        Calendar.Component.hour,
+        Calendar.Component.minute,
+        Calendar.Component.second
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timePrinter), userInfo: nil, repeats: true)
+        
+        timer.fire()
     }
-
+    
+    func timeCalculator(dateFormat: String, endTime: String, startTime: Date = Date()) -> DateComponents {
+        formatter.dateFormat = dateFormat
+        let _startTime = startTime
+        let _endTime = formatter.date(from: endTime)
+        
+        let timeDifference = userCalendar.dateComponents(requestedComponent, from: _startTime, to: _endTime!)
+        return timeDifference
+    }
+    
+    func timePrinter() -> Void {
+        var time = timeCalculator(dateFormat: "MM/dd/yyyy hh:mm:ss a", endTime: "01/15/2017 12:00:00 p")
+        time.hour = (time.day!) * 24 + (time.hour!)
+        timerLabel.text = "\(time.hour!):\(time.minute!):\(time.second!)"
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
