e35db440f656f22df393e0a782638495e007180d
cindyz8735 <cindyz8735@gmail.com>
2017-01-05 14:38:47 -0800

    updates

--- SM Hacks/ViewControllers/UpdatesViewController.swift
+++ SM Hacks/ViewControllers/UpdatesViewController.swift
@@ -7,23 +7,48 @@
 //
 
 import UIKit
-
-var updatesArray = [String]()
-var newUpdate : String = String()
+import Alamofire
+import SwiftyJSON
 
 class UpdatesViewController: UIViewController {
 
+    @IBOutlet var jsonTable: UITableView!
+    
+    // Create array (dictionary)
+    var updatesArray = [[String:AnyObject]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-       
-        let nc = NotificationCenter.default
-        nc.addObserver(forName:myNotification, object:nil, queue:nil, using:catchNotification)
         
-        // add push updates to array
-        // updatesArray.append(newUpdate)
-
+        // Get data with Alamofire
+        Alamofire.request("https://cindyz8735.github.io/ios-app/test-contacts").responseJSON { (responseData) -> Void in
+            if((responseData.result.value) != nil) {
+                let swiftyJsonVar = JSON(responseData.result.value!)
+                
+                if let resData = swiftyJsonVar["contacts"].arrayObject {
+                    self.updatesArray = resData as! [[String:AnyObject]]
+                }
+                if self.updatesArray.count > 0 {
+                    self.jsonTable.reloadData()
+                }
+            }
+        }
+    }
+    
+    // Create Table
+    func tableView(_ tableView: UITableView, cellForRowAtIndexPath indexPath: IndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "jsonCell")!
+        var dict = updatesArray[(indexPath as NSIndexPath).row]
+        cell.textLabel?.text = dict["name"] as? String
+        cell.detailTextLabel?.text = dict["email"] as? String
+        return cell
+    }
+    
+    // Number of cells in table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return updatesArray.count
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,66 +56,6 @@ class UpdatesViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    let myNotification = Notification.Name(rawValue:"MyNotification")
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        let nc = NotificationCenter.default
-        nc.post(name:myNotification,
-                object: nil,
-                userInfo:["message":"Hello there!", "date":Date()])
-    }
-    
-    func catchNotification(notification:Notification) -> Void {
-        print("Catch notification")
-        
-        guard let userInfo = notification.userInfo,
-            let message  = userInfo["message"] as? String,
-            let date     = userInfo["date"]    as? Date else {
-                print("No userInfo found in notification")
-                return
-        }
-        
-        let alert = UIAlertController(title: "Notification!",
-                                      message:"\(message) received at \(date)",
-            preferredStyle: UIAlertControllerStyle.alert)
-        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
-        
-    }
-
-    
-    // how many sections in table
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    // return int = how many rows
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return updatesArray.count
-    }
-    
-    // contents of each cell
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "defaultCell")
-        
-        var updateText = updatesArray[indexPath.row]
-        cell.textLabel?.text = updateText
-        return cell
-    }
-    
-    // give section title
-    /*
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        if section == 0 {
-            return nil
-        }
-        else {
-            return "Sunday, Jan. 15, 2017"
-        }
-    }
-     */
-
     /*
     // MARK: - Navigation
 
